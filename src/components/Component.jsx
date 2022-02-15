9ef68ee7b069e6aabbf4a4a10b2815481a879c98
rafa1st <rafa2nd@gmail.com>
2022-02-15 17:45:37 -0300

    first commit

---
+++ src/components/Component.jsx
@@ -0,0 +1,12 @@
+
+export default function Component () {
+    const counter = (number) => {
+        number = number;
+
+        return number.lenght
+
+
+    };
+    return <h1>Henloo Human</h1>
+    
+}
